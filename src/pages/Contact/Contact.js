0a0a10485a2cb5cdd5c85c7dd2277a2c540a5c1f
Daniel Kaiser <dskaiser82@gmail.com>
2020-03-29 13:19:56 -0700

    styling inout

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -8,17 +8,24 @@ class Contact extends React.Component {
         email: ""
       };
       // This binding is necessary to make `this` work in the callback
-      this.handleEmail = this.handleEmail.bind(this);
+			this.handleEmail = this.handleEmail.bind(this);
+			this.handleSubmit = this.handleSubmit.bind(this);
     } //////////////end constructor//////////////////////
 
     componentDidMount(){
         console.log(this)
-    }
-
+		}
+		//Goes in input
     handleEmail(event){
         this.setState({email: event.target.value})
         console.log(event.target)
     }
+    //Goes om Form
+    handleSubmit(event) {
+        // alert('An essay was submitted: ' + this.state.email);
+        event.preventDefault();
+      }
+    
 
     render() {
       //Per Docs render should not interact with dom
@@ -27,10 +34,17 @@ class Contact extends React.Component {
     
       return (
       <div className="contact">  
-        <h1>Contact Page</h1>
-      <p>{this.props.location.pathname}</p>
-        <input onChange={this.handleEmail} type="email" name="email" />
-      <p>{this.state.email}</p>
+				<div className="container">
+					<h1>Contact Page</h1>
+					<p>{this.props.location.pathname}</p>
+
+					<form onSubmit={this.handleSubmit} >
+							<input onChange={this.handleEmail} type="email" name="email" className="input-email" />
+							<input type="submit" value="Submit" />
+					</form >
+
+					<p>{this.state.email}</p>
+					</div>
       </div>  
       );
     }
