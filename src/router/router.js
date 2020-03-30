3d2b15887e13031757792a75066a7f27b293be47
Daniel Kaiser <dskaiser82@gmail.com>
2020-03-29 18:38:11 -0700

    added get poke

--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import App from '../App';
 import About from '../pages/About/About-class.js';
 import Contact from '../pages/Contact/Contact.js';
+import AllPoke from '../pages/AllPoke/AllPoke.js';
 import { Route } from 'react-router-dom'
 
 function myRoutes() {
@@ -11,6 +12,7 @@ function myRoutes() {
         <Route path="/home" exact component={App} />
         <Route path="/about" exact component={About} />
         <Route path="/contact" exact component={Contact} />
+        <Route path="/poke" exact component={AllPoke} />
         <Route path="/denny" exact component={App} />
     </div>
     );
